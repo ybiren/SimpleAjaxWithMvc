c1d97fdc83a19e624049088507cff490bca41b55
ybiren <37359767+ybiren@users.noreply.github.com>
2018-03-26 14:36:57 +0300

    added cache

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -8,6 +8,8 @@ using WebApplication2.Views.Data;
 using System.Web.Script.Serialization;
 using WebApplication2.Views.Input;
 //using System.Web.Http;
+using System.Runtime.Caching;
+
 
 namespace WebApplication2.Controllers
 {
@@ -15,14 +17,23 @@ namespace WebApplication2.Controllers
 	{ 
 		const int NUM_IMAGES = 2;
 
+
+		private string GetData()
+		{
+			var data = System.IO.File.ReadAllText(Server.MapPath(@"~/App_Data/data.json"));
+			return data;
+		}
+
+
 		public string GetImage(InputView input)
 		{
 			//Server.Transfer("/Home");
 			//var aa = HttpContext.Request.Browser.Type;
+			var cacheService = new InMemoryCache();
 
-		
+			var data = cacheService.GetOrSet("images", () => GetData());
 			
-			var data = System.IO.File.ReadAllText(Server.MapPath(@"~/App_Data/data.json"));
+			//var data = System.IO.File.ReadAllText(Server.MapPath(@"~/App_Data/data.json"));
 			DataView datView = new JavaScriptSerializer().Deserialize<DataView>(data);
 
 			short ind = 0;
@@ -52,4 +63,28 @@ namespace WebApplication2.Controllers
 
 
 	}
+
+
+
+public class InMemoryCache : ICacheService
+	{
+		public T GetOrSet<T>(string cacheKey, Func<T> getItemCallback) where T : class
+		{
+			T item = MemoryCache.Default.Get(cacheKey) as T;
+			if (item == null)
+			{
+				item = getItemCallback();
+				MemoryCache.Default.Add(cacheKey, item, DateTime.Now.AddMinutes(10));
+			}
+			return item;
+		}
+	}
+
+	interface ICacheService
+	{
+		T GetOrSet<T>(string cacheKey, Func<T> getItemCallback) where T : class;
+	}
+
+
+
 }
